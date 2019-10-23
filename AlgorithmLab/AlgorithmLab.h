dab2f5925c8d9e72d87ad9a4c5ed8073973860bb
Connor Tivadar <connortivadar@gmail.com>
2019-10-23 14:33:49 -0700

    AlgortihmLab

--- AlgorithmLab/AlgorithmLab.h
+++ AlgorithmLab/AlgorithmLab.h
@@ -1,9 +1,14 @@
 //
-//  main.c
+//  AlgorithimLab.h
 //  AlgorithmLab
 //
 //  Created by Tivadar, Connor on 10/23/19.
 //  Copyright © 2019 Tivadar, Connor. All rights reserved.
 //
 
+#ifndef AlgorithimLab_h
+#define AlgorithimLab_h
+
 #include <stdio.h>
+
+#endif /* AlgorithimLab_h */
