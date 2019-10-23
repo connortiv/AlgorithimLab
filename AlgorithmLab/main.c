b648159e0a2908ad9c095f350f87bcd3ef6085c3
Connor Tivadar <connortivadar@gmail.com>
2019-10-23 14:25:19 -0700

    Initial Commit

---
+++ AlgorithmLab/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AlgorithmLab
+//
+//  Created by Tivadar, Connor on 10/23/19.
+//  Copyright © 2019 Tivadar, Connor. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
